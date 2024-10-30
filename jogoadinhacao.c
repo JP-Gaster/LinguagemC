f26b12e96b7af4d02dd69c615e29b2043c0ecff9
J. P. Gaster <128156817+JP-Gaster@users.noreply.github.com>
2024-10-30 09:01:12 -0300

    Add files via upload

--- jogoadinhacao.c
+++ jogoadinhacao.c
@@ -7,7 +7,7 @@ int main() {
     printf("*Bem vindo ao nosso jogo de adivinhação*\n");
     printf("****************************************\n");
     
-    int numerosecreto = 15;
+    int numero_secreto = 15;
    // armazenar o chute
     int chute;
         printf("Qual é o seu chute?");
@@ -15,14 +15,16 @@ int main() {
         printf("Seu chute foi %d \n", chute);
         
             //SE O CHUTE FOR IGUAL AO NÚMERO SECRETO
-    if(chute == numerosecreto){
+    if(chute == numero_secreto){
         printf("Parabéns!! Você acertou!!\n");
         printf("Jogue de novo, você é um bom jogador!\n");
     //CASO CONTRÁRIO
     }else{
-        printf("Você errou!!\n");
-        printf("Não desanime, tente novamente! \n");
-        
+        if(chute > numero_secreto){
+            printf("O seu chute foi maior que o número secreto, tente novamente. \n");
+            if(chute < numero_secreto){
+                printf("O seu chute foi menor que o número secreto, tente novamente. \n");
+            }
+        }
     }
-
-    
+}
